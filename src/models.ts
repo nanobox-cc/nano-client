0f16af7b6fc1c00f113a5e9678853e6d8d6f2eb3
Kjetil Mehl <kjetil@mehl.no>
2021-02-16 21:15:15 +0100

    Bootstraps

---
+++ src/models.ts
@@ -0,0 +1,55 @@
+export type NanoAddress = string;
+export type Seed = string;
+export type PrivateKey = string;
+export type PublicKey = string;
+export type Frontier = string;
+export type BlockHash = string;
+
+type ApiError = 'unable-to-fetch';
+
+export interface NanoAccount {
+  alias: string;
+  address: NanoAddress;
+  publicKey: PublicKey;
+  privateKey: PrivateKey;
+  balance: RAW | undefined;
+  representative?: NanoAddress;
+}
+
+export interface NanoWallet {
+  accounts: NanoAccount[];
+  seed: Seed;
+  encryptionSecret: string | undefined;
+}
+
+export interface NanoTransaction {
+  account: NanoAddress;
+  amount: RAW;
+  type: string;
+  localTimestamp: string;
+}
+
+export interface NANO {
+  amount: string;
+}
+
+export interface RAW {
+  raw: string;
+}
+
+export interface AccountInfo {
+  representative: NanoAddress;
+  balance: RAW;
+  frontier: Frontier;
+}
+
+export interface PendingTransaction {
+  hash: BlockHash;
+  amount: RAW;
+}
+
+export interface ResolvedAccount {
+  account: NanoAccount;
+  resolvedCount: number;
+  error?: ApiError;
+}
