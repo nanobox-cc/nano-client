92ae5d5d14684440feb9805006cd5d1ae21c5c2b
Kjetil Mehl <kjetil@mehl.no>
2021-03-05 18:52:58 +0100

    Adds nano ping example

---
+++ examples/nano-ping/README.md
@@ -0,0 +1,14 @@
+# Example: Nano Ping
+
+This is an example of a node app that:
+
+1. Generates an address
+2. The app listens for new transactions for the generated address
+3. As a new transaction happens, this is returned to the sender
+
+## To run
+
+Yarn have to be installed, then in your terminal
+
+    $ yarn 
+    $ yarn run-app
---
+++ examples/nano-ping/index.js
@@ -0,0 +1,29 @@
+import {NanoClient} from "@nanobox/nano-client";
+import qrcode from "qrcode-terminal";
+
+const username = undefined // TODO: Set username
+const password = undefined // TODO: set password
+
+if(!username || !password) {
+    throw new Error('missing username or password config in index.js')
+}
+
+const client = new NanoClient({
+    url: 'https://api.nanobox.cc',
+    credentials: {
+        username: username,
+        password: password
+    },
+    websocketUrl: 'wss://ws.nanobox.cc'
+})
+const account = client.generateWallet().accounts[0]
+console.log(`QR-code for address: ${account.address}`)
+qrcode.generate(account.address);
+console.log(`Send nano to address: ${account.address}`)
+
+client.onReceive(account.address, async received => {
+    console.log(`receives ${received.amount.raw} RAW from ${received.from}`)
+    const updatedAccount = await client.receive(account)
+    console.log(`sends ${updatedAccount.account.balance.raw} RAW to ${received.from}`)
+    await client.sendMax(account, received.from)
+})
