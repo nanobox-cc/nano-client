d7a74546d0e5b82853b59f94397bf87e7e78b4d7
Kjetil Mehl <kjetil@mehl.no>
2021-03-07 10:04:21 +0100

    Uses bignumber as base for calculations

--- src/lib/conversions.ts
+++ src/lib/conversions.ts
@@ -6,11 +6,18 @@ export function rawToNANO(raw: string): string {
     return tools.convert(raw, 'RAW', 'NANO')
 }
 
-export function NANOToRaw(nano: string): string {
-    return tools.convert(nano, 'NANO', 'RAW')
-}
-
 export function NANOToNumber(nano: NANO): number {
     const number = new BigNumber(nano.asString);
     return Number.parseFloat(number.toFixed(number.decimalPlaces()))
+}
+
+export function fromNANOString(nano: string): NANO {
+    return NANO.fromRAW(tools.convert(nano, 'NANO', 'RAW'))
+}
+
+export function addNano(one: NANO, second: NANO): NANO {
+    return fromNANOString(new BigNumber(one.asString).plus(new BigNumber(second.asString)).toString())
+}
+export function subtractNano(one: NANO, second: NANO): NANO {
+    return fromNANOString(new BigNumber(one.asString).minus(new BigNumber(second.asString)).toString())
 }
